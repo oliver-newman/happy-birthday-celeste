95ebbd1b54b1a916d92f2dba75434c5cfd792120
Oliver Newman <oliver@whmsi.com>
2016-11-20 00:35:52 -0500

    Reset number of balloons to normal

--- HappyBirthdayCeleste/static/js/balloons.js
+++ HappyBirthdayCeleste/static/js/balloons.js
@@ -11,8 +11,7 @@ $(document).ready(function() {
     images.push("/static/images/" + colors[i] + "_balloon.png");
   }
 
-  // var numSprites = Math.floor(width / 5);
-  var numSprites = 3;
+  var numSprites = Math.floor(width / 5);
   var sprites = sjs.List();
   var lastSprite;
   scene.loadImages(images, function() {
