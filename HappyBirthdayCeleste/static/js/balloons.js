9dc36a7e66f6105b124c14541da3bd4f87e5e7bd
Oliver Newman <oliver@whmsi.com>
2016-11-22 09:04:03 -0500

    Improve js style

--- HappyBirthdayCeleste/static/js/balloons.js
+++ HappyBirthdayCeleste/static/js/balloons.js
@@ -1,9 +1,9 @@
-var width = Math.min(window.innerWidth, screen.width);
-var height = Math.min(window.innerHeight, screen.height);
-var scene = sjs.Scene({w: width, h: height, autoPause: false});
-
 $(document).ready(function() {
-  playBalloons();
+  const width = Math.min(window.innerWidth, screen.width);
+  const height = Math.min(window.innerHeight, screen.height);
+  const scene = sjs.Scene({w: width, h: height, autoPause: false});
+
+  playBalloons(scene, width, height);
 
   $("#letter-button").click(function() { // Show letter when button is clicked
     $("#letter").slideDown();
@@ -13,52 +13,47 @@ $(document).ready(function() {
   });
   $("#repeat-balloons").click(function() { // Repeat balloon animation
     $("#letter").slideUp(complete = function () {
-      $("#hbd").fadeOut(600, playBalloons)
+      $("#hbd").fadeOut(600, playBalloons(scene, width, height))
     });
   });
 });
 
 
-function playBalloons() {
-  var ticker;
-
+function playBalloons(scene, width, height) {
   document.body.addEventListener("balloonsGone", function () {
     $("#hbd").fadeIn(600);
     scene.reset();
   }, false);
 
-  var colors = ["blue", "green", "orange", "pink", "red", "yellow"];
-  var images = [];
-  for (var i = 0; i < colors.length; i++) {
+  const colors = ["blue", "green", "orange", "pink", "red", "yellow"];
+  let images = [];
+  for (let i = 0; i < colors.length; i++) {
     images.push("/static/images/" + colors[i] + "_balloon.png");
   }
 
-  var numBalloons = Math.floor(width / 8);
-  var balloons = sjs.List([]);
-  var lastBalloon;
-  var balloonsGone;
+  const numBalloons = Math.floor(width / 8);
+  let balloons = sjs.List([]);
+  let balloonsGone = false;
 
   scene.loadImages(images, function() {
-    var filename;
-    var balloon;
-    for (var i = 0; i < numBalloons; i++) {
-      filename = images[i % images.length];
-      
-      balloon = scene.Sprite(filename);
-      var yRand = Math.random();
-      balloon.position((31 * i) % width - 100, height * (1 + yRand*yRand * 1.5));
+    for (let i = 0; i < numBalloons; i++) {
+      const filename = images[i % images.length];
+      const balloon = scene.Sprite(filename);
 
+      const yRand = Math.random();
+      balloon.position((31 * i) % width - 100, height * (1 + yRand*yRand * 1.5));
       balloons.add(balloon);
     }
 
-    balloonsGone = new CustomEvent("balloonsGone");
-    lastBalloon = scene.Sprite(images[0]);
+    const lastBalloon = scene.Sprite(images[0]);
     lastBalloon.position(width / 2, 2.5 * height);
     lastBalloon.update();
     balloons.add(lastBalloon);
 
+    let balloonsGone = new CustomEvent("balloonsGone");
+
     function paint() {
-      var balloon;
+      let balloon = null;
       while(balloon = balloons.iterate()) {
         balloon.update();
         balloon.move(0, -5 + Math.random() * 3);
@@ -68,7 +63,7 @@ function playBalloons() {
       }
     }
 
-    ticker = scene.Ticker(25, paint);
+    const ticker = scene.Ticker(25, paint);
     ticker.run();
   });
 }
